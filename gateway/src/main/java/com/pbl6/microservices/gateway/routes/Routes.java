7035a7bb1b3e794b228dae4bc97a2a444b27fd90
dotuangv <dotuangv@gmail.com>
2025-10-14 23:00:29 +0700

    Import gateway

---
+++ gateway/src/main/java/com/pbl6/microservices/gateway/routes/Routes.java
@@ -0,0 +1,32 @@
+package com.pbl6.microservices.gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.*;
+
+@Configuration
+public class Routes {
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order-service")
+                .route(RequestPredicates.path("api/order/test"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> authServiceRoute() {
+        return GatewayRouterFunctions.route("auth-service")
+                .route(RequestPredicates.path("api/auth/test"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> goongMapServiceRoute() {
+        return GatewayRouterFunctions.route("goongmap-service")
+                .route(RequestPredicates.path("api/goongmap/test"), HandlerFunctions.http("http://localhost:8083"))
+                .build();
+    }
+
+}
