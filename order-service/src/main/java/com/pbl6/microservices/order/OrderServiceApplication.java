0b6a8d882343292049bf0c75ad5e72ae2484bfc4
dotuangv <dotuangv@gmail.com>
2025-10-14 22:55:00 +0700

    Import order-service

---
+++ order-service/src/main/java/com/pbl6/microservices/order/OrderServiceApplication.java
@@ -0,0 +1,16 @@
+package com.pbl6.microservices.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication()
+@EnableFeignClients
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
