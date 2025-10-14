bb76a464550823c4c81fa9c387edb8273b9cbabb
dotuangv <dotuangv@gmail.com>
2025-10-15 00:00:48 +0700

    update new config

--- auth-service/src/main/java/com/pbl6/microservices/auth/client/AuthClient.java
+++ auth-service/src/main/java/com/pbl6/microservices/auth/client/AuthClient.java
@@ -1,12 +1,12 @@
-package com.pbl6.microservices.auth.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@FeignClient(value = "auth", url = "http://localhost:8082")
-public interface AuthClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/auth/account")
-    boolean isActiveAccount(@RequestParam String email);
-}
+//package com.pbl6.microservices.auth.client;
+//
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//
+//@FeignClient(value = "auth", url = "http://localhost:8082")
+//public interface AuthClient {
+//    @RequestMapping(method = RequestMethod.GET, value = "/api/auth/account")
+//    boolean isActiveAccount(@RequestParam String email);
+//}
--- auth-service/src/main/java/com/pbl6/microservices/auth/repository/OrderRepository.java
+++ auth-service/src/main/java/com/pbl6/microservices/auth/repository/OrderRepository.java
@@ -1,6 +1,6 @@
-package com.pbl6.microservices.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface OrderRepository extends JpaRepository<Order,Long> {
-//}
+//package com.pbl6.microservices.auth.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+////public interface OrderRepository extends JpaRepository<Order,Long> {
+////}
