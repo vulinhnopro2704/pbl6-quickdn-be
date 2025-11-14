22c384762a4fb3a4771bbcbc433e34eb8a4b2b7d
Truong Vu Linh <vulinhtruong79@gmail.com>
2025-11-14 21:08:03 +0700

    fix: remove CORS check in nginx

--- gateway/src/main/java/com/pbl6/gateway/routes/Routes.java
+++ gateway/src/main/java/com/pbl6/gateway/routes/Routes.java
@@ -32,22 +32,14 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> goongMapServiceRoute() {
         return GatewayRouterFunctions.route("goongmap-service")
-                .route(RequestPredicates.path("/api/goongmap/test"), HandlerFunctions.http(goongmapServiceUrl))
-                .route(RequestPredicates.path("/api/goongmap/v3/api-docs/**"), HandlerFunctions.http(goongmapServiceUrl))
-                .route(RequestPredicates.path("/api/goongmap/swagger-ui/**"), HandlerFunctions.http(goongmapServiceUrl))
-                .route(RequestPredicates.path("/api/goongmap/swagger-ui.html"), HandlerFunctions.http(goongmapServiceUrl))
+                .route(RequestPredicates.path("/api/goongmap/**"), HandlerFunctions.http(goongmapServiceUrl))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceRoute() {
         return GatewayRouterFunctions.route("order-service")
-                .route(RequestPredicates.path("/api/order/test"), HandlerFunctions.http(orderServiceUrl))
-                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http(orderServiceUrl))
-                // Swagger/OpenAPI endpoints for order-service
-                .route(RequestPredicates.path("/api/order/v3/api-docs/**"), HandlerFunctions.http(orderServiceUrl))
-                .route(RequestPredicates.path("/api/order/swagger-ui/**"), HandlerFunctions.http(orderServiceUrl))
-                .route(RequestPredicates.path("/api/order/swagger-ui.html"), HandlerFunctions.http(orderServiceUrl))
+                .route(RequestPredicates.path("/api/order/**"), HandlerFunctions.http(orderServiceUrl))
                 .build();
     }
 
@@ -55,10 +47,6 @@ public class Routes {
     public RouterFunction<ServerResponse> paymentServiceRoute() {
         return GatewayRouterFunctions.route("payment-service")
                 .route(RequestPredicates.path("/api/payment/**"), HandlerFunctions.http(paymentServiceUrl))
-                // Swagger/OpenAPI endpoints for payment-service
-                .route(RequestPredicates.path("/api/payment/v3/api-docs/**"), HandlerFunctions.http(paymentServiceUrl))
-                .route(RequestPredicates.path("/api/payment/swagger-ui/**"), HandlerFunctions.http(paymentServiceUrl))
-                .route(RequestPredicates.path("/api/payment/swagger-ui.html"), HandlerFunctions.http(paymentServiceUrl))
                 .build();
     }
 }
